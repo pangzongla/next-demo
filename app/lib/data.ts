39ff9a85fde6e3c96718a4afa6cb66ac2adc47c8
庞囧 <980100681@qq.com>
2024-03-14 10:28:01 +0800

    测试

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -7,7 +7,7 @@ type Revenue = {
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-//   noStore();
+  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
